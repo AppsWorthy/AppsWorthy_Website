005f7e93e48c23bf91df05466956f77b7fb3a1c2
AppsWorthy <info@appsworthy.app>
2026-01-23 22:54:10 +0530

    done

---
+++ .next/dev/server/chunks/ssr/Desktop_AppsWorthy_Development_AppsWorthy_Website_app_0dfd4a8a._.js
@@ -0,0 +1,22 @@
+module.exports = [
+"[project]/Desktop/AppsWorthy/Development/AppsWorthy_Website/app/icon.png (static in ecmascript, tag client)", ((__turbopack_context__) => {
+
+__turbopack_context__.v("/_next/static/media/icon.de3cceb1.png");}),
+"[project]/Desktop/AppsWorthy/Development/AppsWorthy_Website/app/icon.png.mjs { IMAGE => \"[project]/Desktop/AppsWorthy/Development/AppsWorthy_Website/app/icon.png (static in ecmascript, tag client)\" } [app-rsc] (structured image object, ecmascript)", ((__turbopack_context__) => {
+"use strict";
+
+__turbopack_context__.s([
+    "default",
+    ()=>__TURBOPACK__default__export__
+]);
+var __TURBOPACK__imported__module__$5b$project$5d2f$Desktop$2f$AppsWorthy$2f$Development$2f$AppsWorthy_Website$2f$app$2f$icon$2e$png__$28$static__in__ecmascript$2c$__tag__client$29$__ = __turbopack_context__.i("[project]/Desktop/AppsWorthy/Development/AppsWorthy_Website/app/icon.png (static in ecmascript, tag client)");
+;
+const __TURBOPACK__default__export__ = {
+    src: __TURBOPACK__imported__module__$5b$project$5d2f$Desktop$2f$AppsWorthy$2f$Development$2f$AppsWorthy_Website$2f$app$2f$icon$2e$png__$28$static__in__ecmascript$2c$__tag__client$29$__["default"],
+    width: 1024,
+    height: 1024
+};
+}),
+];
+
+//# sourceMappingURL=Desktop_AppsWorthy_Development_AppsWorthy_Website_app_0dfd4a8a._.js.map
--- .next/dev/static/chunks/8c925_next_dist_client_components_builtin_global-error_bdf36035.js
+++ .next/dev/static/chunks/8c925_next_dist_client_components_builtin_global-error_bdf36035.js
@@ -1,7 +1,5 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/8c925_next_dist_e04fc37e._.js"
-],
+    chunks: [],
     source: "dynamic"
 });
